7af87620623a6b4bbd43b7210b71e83621dc54cf
Xiang Zhai <zxzhaixiang@gmail.com>
2018-12-19 15:21:02 -0600

    output detection info
    bounding box and other information in detector.c

--- examples/detector.c
+++ examples/detector.c
@@ -620,7 +620,10 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
         int nboxes = 0;
         detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
-        //printf("%d\n", nboxes);
+        printf("%d\n", nboxes);
+	for(int i=0;i<nboxes;i++){
+	   printf("Box %d at (x,y)=(%f,%f) with (w,h)=(%f,%f)", i, dets[i].bbox.x, dets[i].bbox.y, dets[i].bbox.w, dets[i].bbox.h); 
+	}
         //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
         if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
         draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
